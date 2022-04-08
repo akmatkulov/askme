e332010f6ec68487cdf97bc11386ddceb92f901a
Sovetbek Akmatkulov <akmatkulovs991@gmail.com>
2022-04-08 22:52:51 +0300

    Finale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_07_184037) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_08_193445) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
